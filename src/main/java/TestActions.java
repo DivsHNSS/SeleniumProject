a6f1b63208764668a14d01a9e3edceeb0764a449
Divya Jaini <divya.jaini@hnssglobal.com>
2024-04-19 12:45:11 -0400

    Testing CheckBoxes

--- src/main/java/TestActions.java
+++ src/main/java/TestActions.java
@@ -12,33 +12,47 @@ public class TestActions {
 
     @BeforeClass
     public void setup() {
-        driver.get("https://google.com");
+        driver.get("https://www.letskodeit.com/login");
+        driver.manage().window().maximize();
 
     }
+
     @Test
     public void Test1() throws InterruptedException {
         System.out.println("Hello World");
         Thread.sleep(10000);
     }
-        @AfterClass
-        public void close() {
+
+    @AfterClass
+    public void close() {
         driver.close();
-        }
     }
 
+    @Test
+    public void testLogin() throws InterruptedException {
+        driver.findElement(By.xpath("//input[@placeholder='Email Address']")).sendKeys("test@email.com");
+        driver.findElement(By.id("login-password")).sendKeys("abcabc");
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(5000);
+    }
+}
 
-//public class TestActions {
-//    public static void start(String args[]) {
-//         WebDriver driver = new ChromeDriver();
-//
-//        @BeforeClass
-//        public static void start() {
-//            driver.get("https://www.letskodeit.com/login");
-//            driver.manage().window().maximize();
-//
-//        }
-//    }
-//}
+
+
+
+/*
+public class TestActions {
+  public static void start(String args[]) {
+         WebDriver driver = new ChromeDriver();
+
+        @BeforeClass
+        public static void start() {
+            driver.get("https://www.letskodeit.com/login");
+            driver.manage().window().maximize();
+        }
+    }
+}
+*/
 
 
 
